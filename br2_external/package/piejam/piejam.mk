397a512b0070303cc834ac70eb7b97e09cc5b1a1
vilpter <henke.karl@gmail.com>
2026-03-10 18:41:55 -0400

    Update piejam to 79d69cfb (fork+exec instead of posix_spawn)

--- br2_external/package/piejam/piejam.mk
+++ br2_external/package/piejam/piejam.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIEJAM_VERSION = f6ddcbff444b1d4f2e67f1fbd55dc0d897898283
+PIEJAM_VERSION = 79d69cfb06953aa76517fa96c61965c2c7a26a2f
 PIEJAM_SITE = https://github.com/vilpter/piejam.git
 PIEJAM_SITE_METHOD = git
 PIEJAM_GIT_SUBMODULES = YES
